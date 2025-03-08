5fc07d5c2745a91a22c45482d7861f921e7d7498
Yassine <Yassine@gmail.com>
2025-03-08 22:26:32 +0000

    updated (new size)

---
+++ src/main/java/com/example/gestion_stock/LoginController.java
@@ -0,0 +1,40 @@
+package com.example.gestion_stock;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class LoginController {
+    @FXML
+    private AnchorPane leftPane;
+
+    @FXML
+    private AnchorPane rightPane;
+
+    @FXML
+    private Text loginText;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Text emailLabel;
+
+    @FXML
+    private Text passwordLabel;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    public void initialize() {
+
+
+    }
+
+}
