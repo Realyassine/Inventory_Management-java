1ab67062e8613656f10ffde4638328b7a647d4fa
Yassine <Yassine@gmail.com>
2025-03-08 21:38:38 +0000

    Window Size Changed

--- src/main/java/com/example/gestion_stock/HelloApplication.java
+++ src/main/java/com/example/gestion_stock/HelloApplication.java
@@ -10,8 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        // Use classpath-relative path to load the FXML file
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/gestion_stock/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 570, 450);
         stage.setTitle("Login Page");
         stage.setScene(scene);
         stage.show();
