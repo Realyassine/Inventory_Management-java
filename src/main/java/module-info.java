72ee5aefc8fbc7a143faff62c529d2a74ee74f41
Yassine <Yassine@gmail.com>
2025-03-08 21:40:24 +0000

    Same

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.gestion_stock {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.gestion_stock to javafx.fxml;
     exports com.example.gestion_stock;
 }
